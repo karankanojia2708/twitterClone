3b217a6c97312c8fbe03def26e1e87fa9567a6ba
karankanojia2708 <karankanojia2708@gmail.com>
2020-10-23 12:33:14 +0530

    first commit

---
+++ src/main/java/com/example/twitter/clone/twitterclone/controllers/PostController.java
@@ -0,0 +1,9 @@
+package com.example.twitter.clone.twitterclone.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class PostController {
+}
---
+++ src/test/java/com/example/twitter/clone/twitterclone/TwittercloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitter.clone.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittercloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
